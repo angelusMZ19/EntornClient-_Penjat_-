ccfce2762f3b1700e92a7ba3af5cdcff51e6ea10
angelusMZ19 <angelomontenegro200306@gmail.com>
2023-11-09 19:01:23 +0100

    avance

--- js/NOV02_2023.js
+++ js/NOV02_2023.js
@@ -137,3 +137,73 @@ function mostrarEstadisticas() {
 function sortir() {
     console.log("Fins aviat!");
 }
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+function mostrarMenu() {
+    console.log("Benvingut al programa:");
+    console.log("1. Iniciar un joc");
+    console.log("2. Estadístiques");
+    console.log("3. Sortir");
+    let opcio = prompt("Selecciona una opció (1/2/3): ");
+    
+    if (opcio === '1') {
+        iniciarJoc();
+    } else if(opcio === '2') {
+        mostrarEstadisticas();
+    } else if(opcio === '3') {
+        sortir();
+    } else {
+        console.log("Opció no vàlida. Si us plau, selecciona una opció correcta.");
+    }
+}
+
+function iniciarJoc() {
+    console.log("Has seleccionat iniciar un joc.");
+    let palabra = prompt("Ingrese una palabra"); // Ingresa la palabra
+    console.log(("_".repeat(palabra.length)).split(""));
+
+    // Ingresa letra que cree que tiene la palabra
+    let letra = prompt("Ingresa una letra");
+    if (letra.length == 1) {
+        let ascii = letra.toIgnoreCase().charCodeAt(0);
+        if ((ascii >= 65 && ascii <= 91)||(ascii >= 97 && ascii <= 122)) {
+            let word = String.fromCharCode(ascii);
+            console.log("El caracter ingresado " + word + " es una letra.");
+        } else {
+            console.log("El caracter ingresado no es una letra.");
+        }
+    } else {
+        alert("Ingresa una sola letra");
+    }
+    remplazaLetra(letra, palabra);
+}
+function remplazaLetra(letra, palabra){
+    if(palabra.includes(letra)){
+        console.log("si incluye");
+        let nueva="";
+        for (let i = 0; i < palabra.length; i++) {
+            if (palabra[i] === '_') {
+              nueva= palabra[i] = letra;
+            }
+          }
+        console.log(nueva);
+          
+    }else{
+        console.log("no incluye");
+    }
+
+}
+function mostrarEstadisticas() {
+    console.log("Has seleccionat ver les estadístiques.");
+    // Aquí poner el código para mostrar las estadísticas
+    console.log("Cantidad de partidas jugadas");
+    console.log("Cantidad de partidas ganadas %.");
+    console.log("Cantidad de partidas perdidas %.");
+}
+
+function sortir() {
+    console.log("Fins aviat!");
+}
+
+z
